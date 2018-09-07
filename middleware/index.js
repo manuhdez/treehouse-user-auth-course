57bb0572a040bcfea669a35198f94506ef73a26e
Manuel Hernández <manu.hdez.dev@gmail.com>
2018-09-07 14:13:56 +0100

    added middleware to cut access to unlogged users to a certain page

--- middleware/index.js
+++ middleware/index.js
@@ -7,4 +7,15 @@ function loggedOut(req, res, next) {
   return next();
 }
 
+function requiresLogin(req, res, next) {
+  if (req.session && req.session.userId) {
+    return next();
+  } else {
+    const err = new Error('You must be logged in.');
+    err.status = 401;
+    return next(err);
+  }
+}
+
 module.exports.loggedOut = loggedOut;
+module.exports.requiresLogin = requiresLogin;
